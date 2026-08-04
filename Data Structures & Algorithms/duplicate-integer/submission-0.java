44c03bea4f3dc0a94f507164ff3f47d39d624545
SOUMYAJEET SATAPATHY <satapathysoumyajeet@gmail.com>
2026-08-04 22:28:58 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+            for(int num :nums){
+                map.put(num,map.getOrDefault(num,0)+1);
+                if(map.get(num)>=2) return true;
+            } 
+            return false;
+    }
+
+}
